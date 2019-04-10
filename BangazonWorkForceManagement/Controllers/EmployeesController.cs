4904f21860211f8d12294a95dadf940a66ee3fec
unknown <asiamcarter@gmail.com>
2019-04-10 12:03:51 -0500

    details view complete

--- BangazonWorkForceManagement/Controllers/EmployeesController.cs
+++ BangazonWorkForceManagement/Controllers/EmployeesController.cs
@@ -4,6 +4,7 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 using BangazonAPI.Models;
+using BangazonWorkForceManagement.Models.Views;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -75,8 +76,95 @@ namespace BangazonWorkForceManagement.Controllers
         // GET: Employees/Details/5
         public ActionResult Details(int id)
         {
-            return View();
-        }
+            using (SqlConnection conn = Connection)
+
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @" 
+										SELECT e.FirstName AS EmployeeFirstName,
+                                               e.Id AS EmployeeId,
+                                               e.IsSupervisor AS IsSupervisor,
+	                                           e.LastName AS EmployeeLastName,
+                                               d.Budget AS DepartmentBudget,
+	                                           d.Name AS DepartmentName,
+                                               d.Id as DepartmentId,
+											   c.Make as ComputerMake,
+                                               c.Id as ComputerId,
+											   c.PurchaseDate as ComputerPurchaseDate,
+											   c.DecomissionDate as ComputerDecomissionDate,
+											   c.Manufacturer as ComputerManufacturer,
+                                               tp.Id as TrainingProgramId,
+											   tp.Name as TrainingProgramName,
+											   tp.StartDate as TrainingProgramStartDate,
+											   tp.EndDate as TrainingProgramEndDate,
+											   tp.MaxAttendees as TrainingProgramMaxAtendees
+                                        FROM Employee e
+                                        JOIN Department AS d on d.Id = e.DepartmentId
+										JOIN ComputerEmployee AS ce on ce.EmployeeId = e.Id
+										JOIN Computer AS c on c.Id = ce.ComputerId
+										JOIN EmployeeTraining AS et on et.EmployeeId = e.Id
+										JOIN TrainingProgram AS tp on tp.Id = et.TrainingProgramId
+										WHERE e.Id = @id";
+                    cmd.Parameters.Add(new SqlParameter("@Id", id));
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    EmployeeDetailViewModel employee = null;
+                    while (reader.Read())
+                    {
+                        if (employee == null)
+                        {
+                            employee = new EmployeeDetailViewModel
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("EmployeeId")),
+                                FirstName = reader.GetString(reader.GetOrdinal("EmployeeFirstName")),
+                                LastName = reader.GetString(reader.GetOrdinal("EmployeeLastName")),
+                                IsSuperVisor = reader.GetBoolean(reader.GetOrdinal("IsSupervisor")),
+                                DepartmentId = reader.GetInt32(reader.GetOrdinal("DepartmentId")),
+                                Department = new Department
+                                {
+                                    Id = reader.GetInt32(reader.GetOrdinal("DepartmentId")),
+                                    Name = reader.GetString(reader.GetOrdinal("DepartmentName")),
+                                    Budget = reader.GetInt32(reader.GetOrdinal("DepartmentBudget"))
+                                },
+                                Computer = new Computer
+                                {
+                                    Id = reader.GetInt32(reader.GetOrdinal("ComputerId")),
+                                    PurchaseDate = reader.GetDateTime(reader.GetOrdinal("ComputerPurchaseDate")),
+                                    DecommissionDate = reader.GetDateTime(reader.GetOrdinal("ComputerDecomissionDate")),
+                                    Make = reader.GetString(reader.GetOrdinal("ComputerMake")),
+                                    Manufacturer = reader.GetString(reader.GetOrdinal("ComputerManufacturer"))
+                                }
+                            };
+                        }
+                        if (!reader.IsDBNull(reader.GetOrdinal("EmployeeId")))
+                        {
+
+                            if (!reader.IsDBNull(reader.GetOrdinal("TrainingProgramId")))
+                            {
+                                if (!employee.TrainingProgramList.Exists(x => x.Id == reader.GetInt32(reader.GetOrdinal("TrainingProgramId"))))
+                                {
+                                    employee.TrainingProgramList.Add(
+                                new TrainingProgram
+                                {
+                                    Id = reader.GetInt32(reader.GetOrdinal("TrainingProgramId")),
+                                    Name = reader.GetString(reader.GetOrdinal("TrainingProgramName")),
+                                    StartDate = reader.GetDateTime(reader.GetOrdinal("TrainingProgramStartDate")),
+                                    EndDate = reader.GetDateTime(reader.GetOrdinal("TrainingProgramEndDate")),
+                                    MaxAttendees = reader.GetInt32(reader.GetOrdinal("TrainingProgramMaxAtendees"))
+                                });
+                                }
+
+                            }
+                        }
+                    };
+                    reader.Close();
+                    return View(employee);
+                }
+  
+                }
+            }
+        
 
         // GET: Employees/Create
         public ActionResult Create()
---
+++ BangazonWorkForceManagement/Models/Views/EmployeeDetailViewModel.cs
@@ -0,0 +1,32 @@
+﻿using BangazonAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonWorkForceManagement.Models.Views
+{
+    public class EmployeeDetailViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(55, MinimumLength = 2)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(55, MinimumLength = 2)]
+        public string LastName { get; set; }
+
+        [Required]
+        public bool IsSuperVisor { get; set; }
+
+        [Required]
+        public int DepartmentId { get; set; }
+
+        public Department Department { get; set; }
+        public Computer Computer { get; set; }
+        public List<TrainingProgram> TrainingProgramList { get; set; } = new List<TrainingProgram>();
+    }
+}

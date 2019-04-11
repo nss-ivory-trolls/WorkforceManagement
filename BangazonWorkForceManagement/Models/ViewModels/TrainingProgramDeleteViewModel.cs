523028fa87859e3a37b65b1ae27dbd9041f6269c
Joseph <jbaugh78@msn.com>
2019-04-11 15:00:18 -0500

    start delete in controller

---
+++ BangazonWorkForceManagement/Models/ViewModels/TrainingProgramDeleteViewModel.cs
@@ -0,0 +1,13 @@
+﻿using BangazonAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonWorkForceManagement.Models.ViewModels
+{
+    public class TrainingProgramDeleteViewModel
+    {
+        public TrainingProgram TrainingProgram { get; set; }
+    }
+}

abfffd53fd3bc7458babac05da45d9dc52a1ad83
Joseph <jbaugh78@msn.com>
2019-04-15 10:19:36 -0500

    made index view model

---
+++ BangazonWorkForceManagement/Models/ViewModels/TrainingProgramIndexViewModel.cs
@@ -0,0 +1,29 @@
+﻿using BangazonAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonWorkForceManagement.Models.ViewModels
+{
+    public class TrainingProgramIndexViewModel
+    {
+        public TrainingProgram TrainingProgram { get; set; }
+
+        public Boolean UpcomingEvents
+        {
+            get {
+                if (TrainingProgram.StartDate < DateTime.Today)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+
+            }
+
+        }
+    }
+}
